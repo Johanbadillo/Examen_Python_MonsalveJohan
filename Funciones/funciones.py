122929745c7e959600a1031627410eb6766da945
johan <johanbadillomj@gmail.com>
2025-05-22 12:51:45 -0500

    fix: :bug: fx a bug

--- Funciones/funciones.py
+++ Funciones/funciones.py
@@ -3,6 +3,7 @@ def leerDatos(listaRobusta):
             print("=======================================\
                 \n       Dato N.",i+1,"      \
                 \n=======================================")
+            print("Numero del dato:",listaRobusta[i]["numeroDato"])
             print("Nombre:",listaRobusta[i]["nombre"])
             print("descripcion:",listaRobusta[i]["descripcion"])
             print("precio",listaRobusta[i]["precio"])
@@ -13,6 +14,7 @@ def mostrarUno(listaRobusta,Individual):
     print("=======================================")
     print("       Dato N.",Individual)
     print("=======================================")
+    print("Numero del dato:",listaRobusta[Individual-1]["numeroDato"])
     print("Nombre:",listaRobusta[Individual-1]["nombre"])
     print("descripcion:",listaRobusta[Individual-1]["descripcion"])
     print("precio",listaRobusta[Individual-1]["precio"])
